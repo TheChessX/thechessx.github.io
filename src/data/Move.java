78306d75530ec2d9c16eb240979bdab8290e52ab
Jan Buzek <lightning.wolf111@gmail.com>
2019-11-18 09:18:28 -0800

    Manual merge of findMoves into master

--- src/data/Move.java
+++ src/data/Move.java
@@ -1,10 +1,12 @@
+package data;
+
 public class Move {
     final int xInitial;
     final int xFinal;
     final int yInitial;
     final int yFinal;
 
-    public Move(int yInitial, int xInitial, int yFinal, int xFinal) {
+    public Move(int xInitial, int yInitial, int xFinal, int yFinal) {
         this.xInitial = xInitial;
         this.xFinal = xFinal;
         this.yInitial = yInitial;
@@ -15,10 +17,22 @@ public class Move {
     public String toString() { // ASCII 'a' is 97
         StringBuilder standardNotation = new StringBuilder();
         standardNotation.append((char) (xInitial + 97));
-        standardNotation.append(8 - yInitial);
+        standardNotation.append(yInitial + 1);
         standardNotation.append(" to ");
         standardNotation.append((char) (xFinal + 97));
-        standardNotation.append(8 - yFinal);
+        standardNotation.append(yFinal + 1);
         return standardNotation.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        if (((Move)obj).xInitial = xInitial && ((Move)obj).yInitial == yInitial && ((Move)obj).xFinal == xFinal && ((Move)obj).yFinal == yFinal) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
